59e451300c739c90d38665397f9538885fd0c658
haikalnuril <haikal.nuril23@gmail.com>
2024-12-31 20:56:52 +0700

    feat: midtrans notification payment

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -8,13 +8,16 @@ import {
     createOrder,
     currentOrder,
     detailOrder,
+    callbackPayment
 } from "../controllers/orderController.js";
 
 const router = express.Router();
 
-router.post("/", protectedMiddleware, ownerMiddleware, createOrder);
+router.post("/", protectedMiddleware, createOrder);
 router.get("/", protectedMiddleware, ownerMiddleware, allOrder);
 router.get("/:id", protectedMiddleware, ownerMiddleware, detailOrder);
 router.get("/current/user", protectedMiddleware, currentOrder);
+
+router.post("/callback/midtrans", callbackPayment);
 
 export default router;
